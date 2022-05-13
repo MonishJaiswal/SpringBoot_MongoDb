e50d1ffba785225de4b9d33274512fe702027fd6
Monish Jaiswal <monishjaiswal00@gmail.com>
2022-05-13 18:33:25 +0530

    .

--- src/main/java/com/example/demo/reposatory/studentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.reposatory;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.objects.Student;
-
-public interface studentRepo extends MongoRepository<Student, Integer> {
-
-}
